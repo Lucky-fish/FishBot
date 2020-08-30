23ad6e5d186ce6119a54847591999a498a0866f6
Lucky-fish <Lucky_fishOwO@qq.com>
2020-08-31 00:15:45 +0800

    fix again

--- role.fixer.storage.js
+++ role.fixer.storage.js
@@ -31,10 +31,7 @@ module.exports = {
 	           roleHarvester.run(creep);
 	        }
 	    } else {
-			const source = lookForSource.normalFinding(creep);
-			if(creep.harvest(source) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(source);
-            }
+			lookForSource.harvest(creep);
         }
     }
 };
