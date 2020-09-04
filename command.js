64bbe93b1a39e4d28ae1fc1dc9998839dddbf358
Lucky-fish <Lucky_fishOwO@qq.com>
2020-09-04 12:48:11 +0800

    Make dismantler more efficiency.

--- command.js
+++ command.js
@@ -2,7 +2,7 @@ const spawn = require("spawn");
 const commands = {
     dismantle: function(id) {
         spawn.putSpawnTask({role: "dismantler", id: id}, function(s) {
-            return spawn.getBuilderBody(s);
+            return spawn.getHarvesterBody(s);
         });
     }
 };
