7940eb784f1747ae25d2769914d1bb5465dd19c0
Lucky-fish <Lucky_fishOwO@qq.com>
2020-09-08 01:22:25 +0800

    Add claim

--- command.js
+++ command.js
@@ -11,6 +11,14 @@ const commands = {
             return spawn.getBuilderBody(s);
         });
         return "Scheduled."
+    },
+    claim : function(room) {
+        for (let i = 0; i < 2; i ++) {
+            spawn.putSpawnTask({role : "claimer", targetRoomName : room}, function(s) {
+                return spawn.getClaimerBody(spawn);
+            });
+        }
+        return "Scheduled"
     }
 };
 
