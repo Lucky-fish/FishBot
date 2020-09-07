35e0cb4ead2bef77d1cd5f5ae846fcbe1a902cdf
Lucky-fish <Lucky_fishOwO@qq.com>
2020-09-07 10:55:58 +0800

    fix

--- role.miner.js
+++ role.miner.js
@@ -9,7 +9,7 @@ const miner = {
             const room = creep.memory.room;
             const target = Game.getObjectById(creep.memory.target);
 
-            if (!target.room) {
+            if (!target) {
                 const route = Game.map.findRoute(creep.room, room, {
                     routeCallback(roomName) {
                         if (roomName == "E13N29") {
@@ -26,19 +26,21 @@ const miner = {
                 const found = roomManger.find(FIND_MY_STRUCTURES, {filter : function (e) {
                         return e.structureType === STRUCTURE_STORAGE;
                     }});
-                for (let j in found) {
-                    const storage = found[j];
-                    if (storage.store.getFreeCapacity() <= 0) {
-                        continue;
-                    }
 
-                    for (let i in creep.store) {
-                        if (creep.transfer(storage, i) === ERR_NOT_IN_RANGE) {
-                            creep.moveTo(storage);
-                        }
-                    }
-                    return;
+            }
+        } else {
+            for (let j in found) {
+                const storage = found[j];
+                if (storage.store.getFreeCapacity() <= 0) {
+                    continue;
                 }
+
+                for (let i in creep.store) {
+                    if (creep.transfer(storage, i) === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(storage);
+                    }
+                }
+                return;
             }
         }
     }
