782462a178c5c3015c687fd107f765e42a9434c6
Lucky-fish <Lucky_fishOwO@qq.com>
2020-09-08 22:51:39 +0800

    cross-room support.

--- role.repairer.storage.js
+++ role.repairer.storage.js
@@ -11,17 +11,20 @@ const roleUpgrader = require("role.upgrader");
 const lookForSource = require("resource");
 const utils = require("utils");
 const commons = require("commons");
+const roomManager = require("room.manager");
 
 module.exports = {
     run : function(creep) {
 		commons.updateEnergy(creep);
 	    
 	    if (creep.memory.working) {
-			const broken = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (e) => ((e.structureType === STRUCTURE_STORAGE && e.my) || e.structureType === STRUCTURE_CONTAINER) && e.hits < e.hitsMax});
+			const broken = roomManager.find(FIND_STRUCTURES, {filter: function (e) {
+					return ((e.structureType === STRUCTURE_STORAGE && e.my) || e.structureType === STRUCTURE_CONTAINER) && e.hits < e.hitsMax;
+				}});
 
-			if (broken) {
-	            if (creep.repair(broken) === ERR_NOT_IN_RANGE) {
-	                creep.moveTo(broken);
+			if (broken.length) {
+	            if (creep.repair(broken[0]) === ERR_NOT_IN_RANGE) {
+	                creep.moveTo(broken[0]);
 	            }
 	        } else {
 	           roleUpgrader.run(creep);
