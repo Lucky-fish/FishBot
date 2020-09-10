af4927726a882b7dc7e37b8c93c1f11fd1cba4d7
Lucky-fish <Lucky_fishOwO@qq.com>
2020-09-10 17:45:31 +0800

    focus on 1 container.

--- role.hauler.js
+++ role.hauler.js
@@ -46,6 +46,13 @@ const hauler = {
             if (!creep.memory.target) {
                 const found = roomManager.find(FIND_STRUCTURES, {
                     filter: function (target) {
+                        const haulers = _.filter(Game.creeps, (creepw) => creepw.memory.role === "hauler" && creepw !== creep);
+                        for (let i in haulers) {
+                            if (haulers[i].target === target.id) {
+                                return false;
+                            }
+                        }
+
                         return target.structureType == STRUCTURE_CONTAINER && target.store[RESOURCE_ENERGY] > creep.store.getFreeCapacity();
                     }
                 });
@@ -56,14 +63,8 @@ const hauler = {
                 creep.memory.target = found[0].id;
             }
             const target = Game.getObjectById(creep.memory.target);
-            if (target.store[RESOURCE_ENERGY] <= creep.store.getFreeCapacity()) {
-                delete creep.memory.target;
-                return;
-            }
             if (creep.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(target);
-            } else {
-                delete creep.memory.target;
             }
         }
     }
